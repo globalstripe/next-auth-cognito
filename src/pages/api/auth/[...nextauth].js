f2ca1159b846776f672132164d6f051abd4b970e
Chris Clark <chris.clark@globalstripe.com>
2023-11-29 22:06:40 +0000

    first commit 2

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,12 +1,6 @@
 import NextAuth from 'next-auth'
 import CognitoProvider from "next-auth/providers/cognito";
 
-
-// UserPool eu-west-1_PFnsI557R
-// AppClien 6ie96mvnei8lfuqbmm0df8lq0c
-// https://next-auth-signon.auth.eu-west-1.amazoncognito.com
-// Client Secret k2imghehpj68kfcr3gsr2e10b2e3a64j52urk1fhh4edsf5m1h6
-
 export default NextAuth({
   providers: [
     CognitoProvider({
