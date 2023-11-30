6155b790521fdd24017bc36bf7ae566d739067c2
Chris Clark <chris.clark@globalstripe.com>
2023-11-30 11:42:48 +0000

    Updated fix-envs.sh

--- fix-envs.sh
+++ fix-envs.sh
@@ -1,3 +1,5 @@
 # If you want to remove stuff after you have updated .gitignore!
 git rm --cached .env.production
 git rm --cached .env
+git filter-branch --index-filter "git rm -rf --cached --ignore-unmatch .env.produtcion" HEAD
+git push --force
